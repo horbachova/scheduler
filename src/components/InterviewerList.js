ee2de20afcc7f44e12388c2695004a4124c5da31
Zhenia Horbachova <gor.jenia2003@gmail.com>
2023-08-21 02:15:18 -0600

    Refactored InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,27 +3,23 @@ import React from "react";
 import InterviewerListItem from "components/InterviewerListItem"
 import "components/InterviewerList.scss"
 
-export default function InterviewerList({
-	interviewers,
-	interviewer,
-	setInterviewer
-}) {
-	const populateInterviewers = () => {
-		return interviewers.map((interviewer) => (
-			<InterviewerListItem
-				key={interviewer.id}
-				name={interviewer.name}
-				avatar={interviewer.avatar}
-				setInterviewer={setInterviewer}
-				selected={interviewer.id === interviewer}
-			/>
-		));
-	};
-
-	return (
-		<section className="interviewers">
-			<h4 className="interviewers__header text--light">Interviewer</h4>
-			<ul className="interviewers__list">{populateInterviewers()}</ul>
-		</section>
-	);
-}
+export default function InterviewerList(props) {
+    const interviewers = props.interviewers.map(interviewer => {
+      return (
+        <InterviewerListItem 
+          key={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          selected={interviewer.id === props.interviewer}
+          setInterviewer={props.setInterviewer}
+        />
+      )
+    })
+  
+    return (
+      <section className="interviewers">
+        <h4 className="interviewers__header text--light">{interviewers}</h4>
+        <ul className="interviewers__list"></ul>
+      </section> 
+    )
+  }
