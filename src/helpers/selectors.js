35615f99b5628e63df6ee7933d2859571798d2cc
Zhenia Horbachova <gor.jenia2003@gmail.com>
2023-08-21 23:30:54 -0600

    minor vision improvement

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,29 +1,29 @@
 const equalAppointments = (appointments, allId) => {
     const equal = allId.map(id => appointments[id]);
     return equal;
-  }
-  
+}
 
-  function getAppointmentsForDay(state, day) {
+
+function getAppointmentsForDay(state, day) {
     let appointmentArr = [];
     state.days.map(dayObject => {
-      if (dayObject.name === day) {
-        dayObject.appointments.forEach(apptId => appointmentArr.push(apptId))
-      }
+        if (dayObject.name === day) {
+            dayObject.appointments.forEach(apptId => appointmentArr.push(apptId))
+        }
     })
     return equalAppointments(state.appointments, appointmentArr);
-  }
+}
 
-  function getInterview(state, interview) {
+function getInterview(state, interview) {
     if (!interview) {
-      return null;
+        return null;
     }
-  
+
     const interviewerInfo = state.interviewers[interview.interviewer];
     return {
-      student: interview.student,
-      interviewer: interviewerInfo
+        student: interview.student,
+        interviewer: interviewerInfo
     }
 };
 
-  module.exports = { getAppointmentsForDay, getInterview};
+module.exports = { getAppointmentsForDay, getInterview };
