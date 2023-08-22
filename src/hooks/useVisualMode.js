3c1d696ba126aa29ec6a261ace6dd94e67ab7ce1
Zhenia Horbachova <gor.jenia2003@gmail.com>
2023-08-22 05:38:54 -0600

    Styling fixes for hooks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,35 +1,35 @@
 import React, { useState, useEffect } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
+    const [mode, setMode] = useState(initial);
+    const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode, replace = false) => {
-    if (replace) {
-      setMode((prev) => newMode)
-      let replaceArr = [...history];
-      replaceArr[replaceArr.length - 1] = mode;
-      setHistory((prev) => replaceHistory);
-    } else {
-      setMode((prev) => newMode);
-      let newArr = [...history];
-      newArr.push(newMode);
-      setHistory((prev) => newArr);
+    const transition = (newMode, replace = false) => {
+        if (replace) {
+            setMode((prev) => newMode)
+            let replaceArr = [...history];
+            replaceArr[replaceArr.length - 1] = mode;
+            setHistory((prev) => replaceHistory);
+        } else {
+            setMode((prev) => newMode);
+            let newArr = [...history];
+            newArr.push(newMode);
+            setHistory((prev) => newArr);
+        }
+    };
+
+    const back = () => {
+        let newArr = [...history];
+        newArr.pop(mode);
+        setHistory((prev) => newArr);
+        if (history.length > 1) {
+            setMode((prev) => newArr[(newArr.length - 1)]);
+        }
+    };
+
+    return {
+        mode,
+        transition,
+        back
     }
-  };
-
-  const back = () => {
-    let newArr = [...history];
-    newArr.pop(mode);
-    setHistory((prev) => newArr);
-    if (history.length > 1) {
-      setMode((prev) => newArr[(newArr.length - 1)]);
-    }
-  };
-
-  return {
-    mode,
-    transition,
-    back
-  }
 }
